4a378d76226e708c67f4e90a5fcb87f68e0a8505
James Adam Connor <j.adam.connor@gmail.com>
2019-11-19 11:53:59 -0600

    overall visual enhancements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,16 +13,4 @@ export class AppComponent {
 	constructor(
 		public router: Router,
 		public elementRef: ElementRef) {}
-
-	ngAfterViewInit(){
-		this.router.events.subscribe(e => {
-			if (e instanceof NavigationEnd) {
-				if (e.url == '/authenticate') {
-					this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#bcc9d8';
-				} else if (e.url == '/journal') {
-					this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#bcc9d8';
-				}
-			}
-		})
-	}
 }
